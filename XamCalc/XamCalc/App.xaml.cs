6d4392eec480d4a5c90ea3c5e100753eac42546f
Conor King <kingconor@live.co.uk>
2019-07-24 16:22:02 +0100

    ProjectUpload

---
+++ XamCalc/XamCalc/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamCalc.Data;
+using System.IO;
+
+namespace XamCalc
+{
+    public partial class App : Application
+    {
+        static CalcDatabase database;
+
+        public static CalcDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new CalcDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Calculations.db3"));
+                }
+                return database;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ XamCalc/XamCalc/Data/CalcDatabase.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SQLite;
+using XamCalc.Models;
+
+namespace XamCalc.Data
+{
+    public class CalcDatabase
+    {
+        readonly SQLiteAsyncConnection _database;
+
+        public CalcDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Entry>().Wait();
+        }
+
+        public Task<List<Entry>> GetEntriesAsync()
+        {
+            return _database.Table<Entry>().ToListAsync();
+        }
+
+        public Task<Entry> GetEntryAsync(int id)
+        {
+            return _database.Table<Entry>()
+                            .Where(i => i.ID == id)
+                            .FirstOrDefaultAsync();
+        }
+
+        public Task<int> SaveEntryAsync(Entry entry)
+        {
+            if (entry.ID != 0)
+            {
+                return _database.UpdateAsync(entry);
+            }
+            else
+            {
+                return _database.InsertAsync(entry);
+            }
+        }
+
+        public Task<int> DeleteEntryAsync(Entry entry)
+        {
+            return _database.DeleteAsync(entry);
+        }
+    }
+}
